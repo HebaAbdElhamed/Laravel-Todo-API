1731b24ab1593ead153fe91fdd66276d41b163fa
Heba Abd Elhamed <hebaabdelhamede@gmail.com>
2024-11-17 15:06:07 +0200

    Initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            ]);
+
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            ]);
+
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(null, 204);
+    }
+
+    public function destroyAll()
+    {
+        Task::truncate();
+        return response()->json(null, 204);
+    }
+}
